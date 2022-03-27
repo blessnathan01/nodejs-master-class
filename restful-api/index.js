11143687f67f0e6bb97be0af9a155b70b218d572
blessnathan01 <blessingnathaniel01@gmail.com>
2022-03-27 18:39:13 +0300

    Create a server that listens to port 3000 and responds with a text

--- restful-api/index.js
+++ restful-api/index.js
@@ -1,1 +1,23 @@
-console.log("Hello World!");
+/*
+ * Basic file for the API
+ * Author: Blessing Rweikiza
+ * 
+ */
+
+
+// Dependencies
+var http = require('http');
+
+// The server should respond all http requests with a text
+var server = http.createServer(function(req, res) {
+
+    res.end('Hi there!\n');
+
+});
+
+// Start the server that will listen to port 3000
+server.listen(3000, function() {
+
+    console.log("The server is listening to port 3000 now");
+
+});
